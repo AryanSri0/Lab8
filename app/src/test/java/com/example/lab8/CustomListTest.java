b491fa548d4d6c246bef99bb4e2ee42fb0af91ab
“AryanSri0” <aryansrivastava2642@gmail.com>
2023-03-07 18:36:59 -0700

    RED: delete test for delete city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -28,6 +29,14 @@ public class CustomListTest {
         City city = new City("Edmonton", "AB");
         list.addCity(city);
         assertTrue( list.hasCity(city));
+    }
+
+    @Test
+    void testDeleteCity(){
+        CustomList list = new CustomList(null,citylist);
+        City city = new City("Yellowknife", "Northwest Territories");
+        assertThrows( IllegalArgumentException.class, () -> {
+            list.deleteCity(city); });
     }
 
 }
